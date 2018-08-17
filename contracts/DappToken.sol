13a1c6d47674b44a05da19700be87890ed3dece8
JiHyun Kim <jihyunkim@JiHyunui-MacBook-Pro.local>
2018-08-17 16:34:14 +0900

    1. Smoke Test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.17;
+
+contract DappToken {
+
+    uint256 public totalSupply;
+
+    function DappToken() public {
+        totalSupply = 1000000;
+    }
+
+    // set the total number of tokens
+
+    // read th total number of tokens
+}
